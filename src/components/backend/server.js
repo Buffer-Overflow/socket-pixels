9517836d082e95d506995af8ea7ac0c72d07ac60
Jeroen Reurings <info@wickedwarlock.com>
2017-02-21 13:57:19 +0100

    Created separate folders for front-end and back-end components; Moved server logic to back-end component.

---
+++ src/components/backend/server.js
@@ -0,0 +1,67 @@
+'use strict';
+
+const express = require('express');
+const app = express();
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+const chalk = require('chalk')
+const minimist = require('minimist')
+const argv = require('minimist')(process.argv.slice(2));
+
+class Server {
+
+  constructor() {
+    this.port = 8000
+    this.setEnvironment()
+    this.initExpressApp()
+    this.listenForConnections()
+  }
+
+  setEnvironment() {
+    if (!argv.e) {
+      argv.e = 'dev'
+    }
+    console.log(chalk.blue(
+      `☆ Running server in ${argv.e} mode`
+    ));
+  }
+
+  initExpressApp() {
+    app.use(express.static(__dirname + argv.e == 'prod' ? '/../dist' : '/../static'));
+
+    app.get('/', function(req, res) {
+      res.sendFile(__dirname + '/index.html');
+    });
+  }
+
+  listenForConnections() {
+    this.onListen = this.onListen.bind(this)
+    server.listen(this.port, this.onListen);
+  }
+
+  printServerMessage(message) {
+    console.log(chalk.blue('☆ ' + message));
+  }
+
+  printClientMessage(message) {
+    console.log(chalk.green('⚡ ' + message));
+  }
+
+  onListen() {
+    this.printServerMessage('listening on localhost:8000')
+    io.on('connection', (client) => {
+      this.printClientMessage('Client connected')
+      client.on('disconnect', () => {
+        this.printClientMessage('Client disconnected')
+      });
+      client.on('message', (data) => {
+        this.printServerMessage(`Message received with data: ${JSON.stringify(data)}`)
+        client.emit('broadcast', data);
+        client.broadcast.emit('broadcast', data);
+      });
+    });
+  }
+
+}
+
+module.exports = Server;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { render } from 'react-dom'
 import { Router, Route, browserHistory } from 'react-router'
-import App from './components/App'
+import App from './components/frontend/App'
 
 let routes =
   <Router history={browserHistory}>
